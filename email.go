e0ca22f49369d7290e6e9150b0f8830bd2e9fe9a
helei <chanyipiaomiao@163.com>
2018-01-24 16:48:36 +0800

    完善邮件发送功能

--- email.go
+++ email.go
@@ -1,53 +1,82 @@
 package hltool
 
 import (
-	"github.com/jordan-wright/email"
-	"net/smtp"
+	"gopkg.in/gomail.v2"
 )
 
+// Message 内容
+type Message struct {
+	From 		string
+	To 			[]string
+	Cc  		[]string
+	Subject 	string
+	ContentType string
+	Content 	string
+	Attach 		string
+}
 
-// Auth 验证
-type Auth struct {
+// NewMessage 返回消息对象
+// from: 发件人
+// subject: 标题
+// contentType: 内容的类型 text/plain text/html
+// attach: 附件
+// to: 收件人
+// cc: 抄送人
+func NewMessage(from, subject, contentType, content, attach string, to, cc []string) *Message{
+	return &Message{
+		From: from,
+		Subject: subject,
+		ContentType: contentType,
+		Content: content,
+		To: to,
+		Cc: cc,
+		Attach: attach,
+	}
+}
+
+// Client 发送客户端
+type Client struct {
 	Host 	string
-	Port	string
+	Port	int
 	Username string
 	Password string
-	Identity string
 }
 
-// Email 发送邮件
-type Email struct {
-	From 	string
-	To 		[]string
-	Bcc 	[]string
-	Cc  	[]string
-	Subject string
-	Text 	[]byte
-	HTML 	[]byte
-	Auth    *Auth
-}
-
-// NewEmail 初始化 email
-func NewEmail() *Email{
-	return new(Email)
+// NewClient 返回一个邮件客户端 
+// host smtp地址
+// username 用户名
+// password 密码
+// port 端口
+func NewClient(host, username, password string, port int) *Client{
+	return &Client{
+		Host: host,
+		Port: port,
+		Username: username,
+		Password: password,
+	}
 }
 
 // Send 发送邮件
-func (e *Email) Send(){
-	m := email.NewEmail()
-	m.From = e.From
-	m.To = e.To
-	m.Subject = e.Subject
+func (c *Client) Send(m *Message) (bool, error) {
 
-	if len(e.Text) != 0 {
-		m.Text = e.Text
+	e := gomail.NewDialer(c.Host, c.Port, c.Username, c.Password)
+	dm := gomail.NewMessage()
+	dm.SetHeader("From", m.From)
+	dm.SetHeader("To", m.To...)
+
+	if len(m.Cc) != 0 {
+		dm.SetHeader("Cc", m.Cc...)
 	}
 
-	if len(e.HTML) != 0 {
-		m.HTML = e.HTML
+	dm.SetHeader("Subject", m.Subject)
+	dm.SetBody(m.ContentType, m.Content)
+
+	if m.Attach != "" {
+		dm.Attach(m.Attach)
 	}
-
-	m.Send(e.Auth.Host + ":" + e.Auth.Port, smtp.PlainAuth(e.Auth.Identity, e.Auth.Username, e.Auth.Password, e.Auth.Host))
-
-	return
+	
+	if err := e.DialAndSend(dm); err != nil {
+		return false, err
+	}
+	return true, nil
 }
