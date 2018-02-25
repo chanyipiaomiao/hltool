018c3a39cf775a4fc5cabc6f61266e2f759b531d
helei <411765588@qq.com>
2018-02-25 19:29:57 +0800

    更新产生随机字符串功能

--- secret.go
+++ secret.go
@@ -1,43 +1,64 @@
 package hltool
 
 import (
+	"crypto/md5"
+	"encoding/hex"
 	"fmt"
 	"math/rand"
-	"time"
-	"encoding/hex"
-	"crypto/md5"
+
 	"golang.org/x/crypto/scrypt"
 )
 
-// GetRandomString 生成随机字符串
+// var (
+// 	chars   = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+// 	special = "!@#%$*.="
+// )
+
+// GenRandomString 生成随机字符串
+// chars  指定的字符串
 // length 长度
-// specialchar 是否包含特殊字符
-func GetRandomString(length int, specialchar string) string{
-	str := "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-	special := "!@#%$*.="
+func GenRandomString(length int, chars []byte) string {
 
-	if specialchar == "yes" {
-		str = str + special
+	if length == 0 {
+		return ""
 	}
 
-	bytes := []byte(str)
-	result := []byte{}
-	r := rand.New(rand.NewSource(time.Now().UnixNano()))
-	for i := 0; i < length; i++ {
-	   result = append(result, bytes[r.Intn(len(bytes))])
+	clen := len(chars)
+	if clen < 2 || clen > 256 {
+		panic("Wrong charset length for NewLenChars()")
 	}
-	return string(result)
- }
 
- // CryptPassword 加密密码
+	maxrb := 255 - (256 % clen)
+	b := make([]byte, length)
+	r := make([]byte, length+(length/4)) // storage for random bytes.
+	i := 0
+
+	for {
+		if _, err := rand.Read(r); err != nil {
+			panic("Error reading random bytes: " + err.Error())
+		}
+		for _, rb := range r {
+			c := int(rb)
+			if c > maxrb {
+				continue // Skip this number to avoid modulo bias.
+			}
+			b[i] = chars[c%clen]
+			i++
+			if i == length {
+				return string(b)
+			}
+		}
+	}
+}
+
+// CryptPassword 加密密码
 func CryptPassword(password, salt string) string {
 	dk, _ := scrypt.Key([]byte(password), []byte(salt), 16384, 8, 1, 32)
 	return fmt.Sprintf("%x", dk)
 }
 
-
 // GetMD5 生成32位MD5
-func GetMD5(text string) string{
+func GetMD5(text string) string {
 	ctx := md5.New()
 	ctx.Write([]byte(text))
 	return hex.EncodeToString(ctx.Sum(nil))
