e6864dba0687fef1db10adc32b02c2a94883f21a
helei <chanyipiaomiao@163.com>
2018-04-20 14:38:38 +0800

    完善 rsa加密库

--- rsa.go
+++ rsa.go
@@ -2,20 +2,24 @@ package hltool
 
 import (
 	"bytes"
+	"crypto"
 	"crypto/rand"
 	"crypto/rsa"
 	"crypto/x509"
 	"encoding/asn1"
+	"encoding/base64"
 	"encoding/pem"
+	"errors"
 	"io"
+	"io/ioutil"
 	"os"
 )
 
-// GoRSA RSA加密解密
-type GoRSA struct {
-	PublicKey  *rsa.PublicKey
-	PrivateKey *rsa.PrivateKey
-}
+const (
+
+	// RSAAlgorithmSign RSA签名算法
+	RSAAlgorithmSign = crypto.SHA256
+)
 
 // NewRSAFile 生成密钥对文件
 // pubKeyFilename: 公钥文件名 priKeyFilename: 私钥文件名 kekeyLength: 密钥长度
@@ -27,8 +31,8 @@ func NewRSAFile(pubKeyFilename, priKeyFilename string, keyLength int) error {
 		priKeyFilename = "id_rsa"
 	}
 
-	if keyLength == 0 {
-		keyLength = 2048
+	if keyLength == 0 || keyLength < 1024 {
+		keyLength = 1024
 	}
 
 	// 创建公钥文件
@@ -46,7 +50,7 @@ func NewRSAFile(pubKeyFilename, priKeyFilename string, keyLength int) error {
 	defer priWriter.Close()
 
 	// 生成密钥对
-	err = CreateKeyPair(pubWriter, priWriter, keyLength)
+	err = WriteRSAKeyPair(pubWriter, priWriter, keyLength)
 	if err != nil {
 		return err
 	}
@@ -58,8 +62,8 @@ func NewRSAFile(pubKeyFilename, priKeyFilename string, keyLength int) error {
 // keyLength 密钥的长度
 func NewRSAString(keyLength int) (string, string, error) {
 
-	if keyLength == 0 {
-		keyLength = 2048
+	if keyLength == 0 || keyLength < 1024 {
+		keyLength = 1024
 	}
 
 	bufPub := make([]byte, 1024*5)
@@ -68,7 +72,7 @@ func NewRSAString(keyLength int) (string, string, error) {
 	bufPri := make([]byte, 1024*5)
 	pribuffer := bytes.NewBuffer(bufPri)
 
-	err := CreateKeyPair(pubuffer, pribuffer, keyLength)
+	err := WriteRSAKeyPair(pubuffer, pribuffer, keyLength)
 	if err != nil {
 		return "", "", nil
 	}
@@ -76,26 +80,8 @@ func NewRSAString(keyLength int) (string, string, error) {
 	return pubuffer.String(), pribuffer.String(), nil
 }
 
-// MarshalPKCS8PrivateKey 私钥解析
-func MarshalPKCS8PrivateKey(key *rsa.PrivateKey) []byte {
-
-	info := struct {
-		Version             int
-		PrivateKeyAlgorithm []asn1.ObjectIdentifier
-		PrivateKey          []byte
-	}{}
-
-	info.Version = 0
-	info.PrivateKeyAlgorithm = make([]asn1.ObjectIdentifier, 1)
-	info.PrivateKeyAlgorithm[0] = asn1.ObjectIdentifier{1, 2, 840, 113549, 1, 1, 1}
-	info.PrivateKey = x509.MarshalPKCS1PrivateKey(key)
-	k, _ := asn1.Marshal(info)
-	return k
-
-}
-
-// CreateKeyPair 生成RSA密钥对
-func CreateKeyPair(publicKeyWriter, privateKeyWriter io.Writer, keyLength int) error {
+// WriteRSAKeyPair 生成RSA密钥对
+func WriteRSAKeyPair(publicKeyWriter, privateKeyWriter io.Writer, keyLength int) error {
 
 	// 生成私钥文件
 	privateKey, err := rsa.GenerateKey(rand.Reader, keyLength)
@@ -133,4 +119,155 @@ func CreateKeyPair(publicKeyWriter, privateKeyWriter io.Writer, keyLength int) e
 
 	return nil
 
+}
+
+// ReadRSAKeyPair 读取RSA密钥对
+// pubKeyFilename: 公钥文件名称   priKeyFilename: 私钥文件名
+func ReadRSAKeyPair(pubKeyFilename, priKeyFilename string) ([]byte, []byte, error) {
+	pub, err := ioutil.ReadFile(pubKeyFilename)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	pri, err := ioutil.ReadFile(priKeyFilename)
+	if err != nil {
+		return nil, nil, err
+	}
+	return pub, pri, nil
+}
+
+// GoRSA RSA加密解密
+type GoRSA struct {
+	PublicKey  *rsa.PublicKey
+	PrivateKey *rsa.PrivateKey
+}
+
+// NewGoRSA 初始化 GoRSA对象
+func NewGoRSA(pubKeyFilename, priKeyFilename string) (*GoRSA, error) {
+
+	publicKey, privateKey, err := ReadRSAKeyPair(pubKeyFilename, priKeyFilename)
+	if err != nil {
+		return nil, err
+	}
+
+	block, _ := pem.Decode(publicKey)
+	if block == nil {
+		return nil, errors.New("public key error")
+	}
+
+	pubInterface, err := x509.ParsePKIXPublicKey(block.Bytes)
+	if err != nil {
+		return nil, err
+	}
+	pub := pubInterface.(*rsa.PublicKey)
+	block, _ = pem.Decode(privateKey)
+	if block == nil {
+		return nil, errors.New("private key error")
+	}
+
+	priv, err := x509.ParsePKCS8PrivateKey(block.Bytes)
+	if err != nil {
+		return nil, err
+	}
+
+	pri, ok := priv.(*rsa.PrivateKey)
+	if ok {
+		return &GoRSA{
+			PublicKey:  pub,
+			PrivateKey: pri,
+		}, nil
+	}
+	return nil, errors.New("private key not supported")
+}
+
+// PublicEncrypt 公钥加密
+func (r *GoRSA) PublicEncrypt(data string) (string, error) {
+	partLen := r.PublicKey.N.BitLen()/8 - 11
+	chunks := split([]byte(data), partLen)
+	buffer := bytes.NewBufferString("")
+
+	for _, chunk := range chunks {
+		bytes, err := rsa.EncryptPKCS1v15(rand.Reader, r.PublicKey, chunk)
+		if err != nil {
+			return "", err
+		}
+		buffer.Write(bytes)
+	}
+	return base64.RawURLEncoding.EncodeToString(buffer.Bytes()), nil
+
+}
+
+// PrivateDecrypt 私钥解密
+func (r *GoRSA) PrivateDecrypt(encrypted string) (string, error) {
+
+	partLen := r.PublicKey.N.BitLen() / 8
+	raw, err := base64.RawURLEncoding.DecodeString(encrypted)
+	chunks := split([]byte(raw), partLen)
+	buffer := bytes.NewBufferString("")
+
+	for _, chunk := range chunks {
+		decrypted, err := rsa.DecryptPKCS1v15(rand.Reader, r.PrivateKey, chunk)
+		if err != nil {
+			return "", err
+		}
+		buffer.Write(decrypted)
+	}
+	return buffer.String(), err
+}
+
+// Sign 数据进行签名
+func (r *GoRSA) Sign(data string) (string, error) {
+
+	h := RSAAlgorithmSign.New()
+	h.Write([]byte(data))
+	hashed := h.Sum(nil)
+	sign, err := rsa.SignPKCS1v15(rand.Reader, r.PrivateKey, RSAAlgorithmSign, hashed)
+	if err != nil {
+		return "", err
+	}
+	return base64.RawURLEncoding.EncodeToString(sign), err
+}
+
+// Verify 数据验证签名
+func (r *GoRSA) Verify(data string, sign string) error {
+
+	h := RSAAlgorithmSign.New()
+	h.Write([]byte(data))
+	hashed := h.Sum(nil)
+	decodedSign, err := base64.RawURLEncoding.DecodeString(sign)
+	if err != nil {
+		return err
+	}
+	return rsa.VerifyPKCS1v15(r.PublicKey, RSAAlgorithmSign, hashed, decodedSign)
+}
+
+// MarshalPKCS8PrivateKey 私钥解析
+func MarshalPKCS8PrivateKey(key *rsa.PrivateKey) []byte {
+
+	info := struct {
+		Version             int
+		PrivateKeyAlgorithm []asn1.ObjectIdentifier
+		PrivateKey          []byte
+	}{}
+
+	info.Version = 0
+	info.PrivateKeyAlgorithm = make([]asn1.ObjectIdentifier, 1)
+	info.PrivateKeyAlgorithm[0] = asn1.ObjectIdentifier{1, 2, 840, 113549, 1, 1, 1}
+	info.PrivateKey = x509.MarshalPKCS1PrivateKey(key)
+	k, _ := asn1.Marshal(info)
+	return k
+
+}
+
+func split(buf []byte, lim int) [][]byte {
+	var chunk []byte
+	chunks := make([][]byte, 0, len(buf)/lim+1)
+	for len(buf) >= lim {
+		chunk, buf = buf[:lim], buf[lim:]
+		chunks = append(chunks, chunk)
+	}
+	if len(buf) > 0 {
+		chunks = append(chunks, buf[:len(buf)])
+	}
+	return chunks
 }
